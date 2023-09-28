df98cb8dbf1d31695dcdf41520a6f7ca582b5f41
Ojiro <ochanmoy@gmail.com>
2023-09-28 17:59:08 -0400

    freestyle

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,8 +1,7 @@
+import java.util.Random;
 import java.util.Scanner;
-import java.text.DecimalFormat;
 public class TipCalculator {
     public static void main(String[] args) {
-        DecimalFormat df = new DecimalFormat("0.00");
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome");
         System.out.print("Number of people: ");
@@ -14,20 +13,28 @@ public class TipCalculator {
         while (cost != -1) {
             System.out.print("Cost of item (-1 to end): $");
             cost = scan.nextDouble();
-            System.out.print("Number of item: ");
-            int numItem = scan.nextInt();
-            if (cost != -1) {
-                totalCost += cost*numItem;
-            }
+            totalCost += cost;
         }
+        System.out.println("------------------------");
         double tip = totalCost*(tipPercent/100.0);
         double totalBill = totalCost + tip;
         System.out.println("Total bill (before tip): $" + String.format("%.2f",totalCost));
         System.out.println("Tip Percentage: " + tipPercent + "%");
         System.out.println("Tip Amount: $" + String.format("%.2f",tip));
-        System.out.println("Total bill: $" + String.format("%.2f",totalBill));
-        System.out.println("Cost per person (before tip): $" + String.format("%.2f",totalCost/numPeople));
-        System.out.println("Tip per person: $" + String.format("%.2f",tip/numPeople));
-        System.out.println("Cost per person: $" + String.format("%.2f",totalBill/numPeople));
+        System.out.println("Total Bill: $" + String.format("%.2f",totalBill));
+        System.out.println("Cost Per Person (before tip): $" + String.format("%.2f",totalCost/numPeople));
+        System.out.println("Tip Per Person: $" + String.format("%.2f",tip/numPeople));
+        System.out.println("Cost Per Person: $" + String.format("%.2f",totalBill/numPeople));
+        //FREESTYLE
+        Random random = new Random();
+        double balance = random.nextDouble(400);
+        System.out.println("------------------------");
+        if (totalBill > balance) {
+            System.out.println("CARD ACCEPTED");
+            System.out.println("$" + String.format("%.2f",(totalBill - balance)) + " remaining");
+        } else {
+            System.out.println("CARD DECLINED");
+            System.out.println("$" + String.format("%.2f",(balance - totalBill)) + " required");
+        }
     }
 }
